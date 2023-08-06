7136074761dd9526dfb7089c995e4ef3748659e0
Michał Łysak <michal.piotr.lysak@gmail.com>
2023-08-06 19:35:09 +0200

    Add cors handling and create README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+# KanbanBoardExpress
+
+API for [github.com/michallysak/kanban-board-angular](https://github.com/michallysak/kanban-board-angular)
+
+## Build
+
+Run `npm install` and then `npm run compile` to build the project. The build artifacts will be stored in the `dist/` directory.
+
+## Running server
+
+Run `npm run server` to run server in `localhost:3001`
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Application, Request, Response } from 'express';
+import cors from 'cors'
 import { initialKanbanBoardColumn } from './data';
 
 const app: Application = express();
@@ -6,6 +7,20 @@ const port: number = 3001;
 const apiRoot = '/api';
 
 const kanbanBoardColumns = [...initialKanbanBoardColumn];
+
+const allowedOrigins = ['http://localhost:4200'];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  })
+);
 
 app.get(`${apiRoot}/table`, (req: Request, res: Response) => {
   const tables = kanbanBoardColumns.map(({ id, name }) => ({ id, name }));
@@ -24,3 +39,4 @@ app.get(`${apiRoot}/table/:id/task`, (req: Request, res: Response) => {
 app.listen(port, function () {
   console.log(`App is listening on port ${port} !`);
 });
+
